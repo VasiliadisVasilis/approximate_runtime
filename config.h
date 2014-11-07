7843d75dbe29f778baf95f0520f1300bdfc049ea
koparasy <koparasy@ubuntu.(none)>
2014-11-07 09:28:50 -0800

    changes to support fault injection using gemfi

---
+++ config.h
@@ -0,0 +1,39 @@
+#ifndef __CONFIG__
+#define __CONFIG__
+
+
+
+#include "m5op.h"
+
+
+
+#ifdef GEMFI 						
+#ifdef DUAL_TASKS 
+#define ENABLE_FI(val)					\
+	if ( val -> significance == Non_Reliable )		\
+		fi_activate_inst(val->task_id, START); 	\
+
+#define DISABLE_FI(val) 				\
+	if( val -> significance == Non_Reliable )		\
+		fi_activate_inst(val->task_id, PAUSE); 	\
+
+#else
+
+#define ENABLE_FI(val) 	fi_activate_inst(val->task_id, START);
+#define DISABLE_FI(val) fi_activate_inst(val->task_id, PAUSE); 
+
+#endif
+
+#define INIT_FI(a,b) fi_read_init_all((unsigned long)&a, (unsigned long)&b);	
+#define STOP_FI() fi_activate_inst(0,DUMP);			
+
+#else
+
+#define ENABLE_FI(val) ;
+#define DISABLE_FI(val) ;
+#define STOP_FI() ;
+#define INIT_FI(a,b) ; printf(" 0x%lx 0x%lx\n",(unsigned long)&a, (unsigned long) &b);
+
+#endif
+
+#endif

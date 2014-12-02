b313bd52db0090399527c8ff3a614f665617cc67
Vasiliadis Vasilios <vasiliad@uth.gr>
2014-12-02 12:27:55 +0200

    Updated classify for the Jacobi benchmark

--- benchmarks/jacobi/classify.c
+++ benchmarks/jacobi/classify.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
@@ -9,49 +10,33 @@ int main(int argc, char *argv[])
   double err1, err2;
   int identical = 1;
   int i = 0;
+  
 
   if ( argc != 5 )
   {
-    printf("%s File1 File2 Iterations CorrectIterations\n", argv[0]);    
+    printf("%s Diff CorrectDiff Iterations CorrectIterations\n", argv[0]);    
     return 1;
   }
-  
-  input1 = fopen(argv[1], "rb");
-  input2 = fopen(argv[2], "rb");
-  identical = atoi(argv[3]) == atoi(argv[4]);
-  err1 = 0;
-  err2 = 0;
-  rel_err = 0;
 
-  while( fread(&d, sizeof(double), 1, input1) 
-    && fread(&e, sizeof(double), 1, input2))
-  {
-    if ( err1 < fabs(d-1.0) )
-    {
-      err1 = fabs(d-1.0);
-    }
-    if ( err2 < fabs(e-1.0) )
-    {
-      err2 = fabs(e-1.0);
-    }
-    
-    identical = ( *((long*)&d)==*((long*)&e)) ? identical : 0;
-  }
+  err1 = strtod(argv[1], NULL);
+  err2 = strtod(argv[2], NULL);
   
   rel_err = fabs(err2-err1)/err2;
   
-//  printf("Rel Error: %lf\nErr1: %lf\nErr2: %lf\n", rel_err, err1, err2);
-//  printf("Verdict: ");
+//  printf("[%lg %lg] %lf : ", err1, err2, rel_err);
+  
+  identical = (err1 == err2);
+
   if ( identical )
   {
-    printf("strict\n");
+    printf("strict");
   } else if ( err1 < err2 || rel_err < 0.1f  )
   {
-    printf("correct\n");
+    printf("correct");
   }
   else
   {
-    printf("altered\n");
+    printf("altered");
   }
   return 0;
 }

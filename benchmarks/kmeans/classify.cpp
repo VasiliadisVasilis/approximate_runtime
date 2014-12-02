47be4927cd452357de6ad3abaa955d93c53edf7b
Vasiliadis Vasilios <vasiliad@uth.gr>
2014-12-02 20:15:33 +0200

    Updated the KMeans execution classifier, looks better now

--- benchmarks/kmeans/classify.cpp
+++ benchmarks/kmeans/classify.cpp
@@ -2,13 +2,15 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 
 int main(int argc, char* argv[])
 {
   std::map<int, std::map<int,int> > edges[2];
   std::vector<int>  memberships[2];
+  std::map<int,int>  populations;
   int points, points2;
-  int p1, p2, i, j;
+  int p1, p2, i, j, faults;
 
   if ( argc != 3 )
   {
@@ -36,11 +38,29 @@ int main(int argc, char* argv[])
     memberships[0].push_back(p1);
     input[1].read((char*)&p2, sizeof(int));
     memberships[1].push_back(p2);
+
+    populations[p2] = populations[p2] +1;
   }
 
   input[0].close();
   input[1].close();
-  
+
+#if 0
+  /* This simulates faults during the execution ... cheap but seems to be working */
+  faults = 0;
+  srand(time(0));
+  int err_rate = rand()%101;
+  for ( i =0; i<points; ++i )
+  {
+    if ( rand()%100 < err_rate )
+    {
+      int e = rand()% 5;
+      if ( e !=  memberships[0][i] )
+        faults ++;
+      memberships[0][i] = e;
+    }
+  }
+#endif
   for ( i=0; i<points; ++i)
   {
     int m = memberships[0][i];
@@ -64,8 +84,8 @@ int main(int argc, char* argv[])
     }
   }
 
-  int err = 0;
-  int total = 0;
+  double err = 0;
+  double total = 0;
   double rel_err = 0;
 
 
@@ -73,12 +93,13 @@ int main(int argc, char* argv[])
   {
     for (j=0; j<points; ++j)
     {
-      total += edges[1][i][j];
-      err += ( (edges[0][i][j] != edges[1][i][j]) && i>j);
+      total += edges[1][i][j]/(float)populations[memberships[1][i]];
+      err += ( (edges[0][i][j] != edges[1][i][j]) && i>j)
+          /(float)populations[memberships[1][i]];
     }
   }
   
-  #if 1
+  #if 0
   for ( i=0; i<points; ++i)
   {
     for (j=0; j<points; ++j)
@@ -92,7 +113,9 @@ int main(int argc, char* argv[])
 
   rel_err = err*100.0/(double)(total);
   std::cout << "Err: " << err << " out of " << total << " (" << rel_err << "%)" << std::endl;
-
-
+#if 0
+  std::cout << "Faults: " << faults << std::endl; 
+  std::cout << "Ratio: " << err/faults << std::endl;
+#endif
   return 0;
 }
